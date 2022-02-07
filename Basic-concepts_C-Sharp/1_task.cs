2ab1573b580676695539dd3cd756b2bc098c135c
nasibullo <nasibullo.khalilov@bk.ru>
2022-02-07 10:49:57 +0700

    1 задача

---
+++ Basic-concepts_C-Sharp/1_task.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Basic_concepts_C_Sharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+            int n, a, result;
+
+
+
+            Console.Write("Введите число a:");
+            a = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Введите число n:");
+            n = Convert.ToInt32(Console.ReadLine());
+            result = a;
+            
+            for (int step = 0; step <n; step++)
+            {
+                result *= a;
+            }
+
+            Console.WriteLine(result);
+            
+
+        }
+    }
+}
